54f632f20ad7c50d30b7ee711a94bb7d18dc03b1
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-09-07 16:35:22 -0400

    adds solution

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+
+function retrieveEmployeeInformation(){
+  return input.value
+}
+
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
