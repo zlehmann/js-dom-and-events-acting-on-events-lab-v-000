2949537cd6dab56b8925720b91fddb232ce87c80
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-09-07 16:26:18 -0400

    new index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much."
+function preventRefreshOnSubmit(){
+    document.querySelector('form').addEventListener('submit', function(event){
+      event.preventDefault()
+    })
+}
+
+preventRefreshOnSubmit()
