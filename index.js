11545ac4f662c446c2979bd93b87b22f44f22e52
Zachary Lehmann <zlehmann@gmail.com>
2019-03-29 20:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return $('input').value;
+  return document.querySelectorAll('input').value;
 }
