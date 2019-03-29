2e9093bacb2cf8851bc42c31a862d03df8553804
Zachary Lehmann <zlehmann@gmail.com>
2019-03-29 20:49:46 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  document.querySelector('.employee-list').append(retrieveEmployeeInformation());
+}
+
+function addNewLiOnClick() {
+  const button = document.querySelector('input');
+  button.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  const link = document.querySelector('a');
+  link.addEventListener('click', function(e) {
+    document.querySelector('.employee-list').innerText = "";
+  });
+}
